8b7921f0e277fafdf9713590d41eb70edac2d7e5
ono <insidetail3@gmail.com>
2021-05-03 17:23:25 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "prototypes#index"
+  # get 'prototypes', to: 'prototypes#index'
+  
+  resources :prototypes do
+    resources :comments, only: :create
+    # collection do
+    #   get 'search'
+    # end
+  end
+  resources :users, only: :show
+  
+  #   resources :comments, only: :create
+  #   collection do
+  #     get 'search'
+  #   end
+  # end
+  # resources :users, only: :show
+
+
+
+
+
+
+  # resources :prototypes, only: :index
+
+
+end
